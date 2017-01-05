9b05b6e7d908f70ca6dd31209cfbf49c33e55a07
Nathan Kuenzig <kuenzign@gmail.com>
2017-01-04 20:00:25 -0500

    Added template code from Simon Brown http://forums.getpaint.net/index.php?/topic/7984-filetype-plugin-template-v20/

---
+++ TIPic/Code.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using PaintDotNet;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace TIPic
+{
+    public class MyFileType : FileType
+    {
+        public MyFileType()
+            : base("Text Document",
+                FileTypeFlags.SupportsLoading | FileTypeFlags.SupportsSaving,
+                new String[] { ".txt" })
+        {
+        }
+
+        protected override Document OnLoad(Stream input)
+        {
+            try
+            {
+                Bitmap b = new Bitmap(500, 500);
+
+                return Document.FromImage(b);
+            }
+            catch
+            {
+                MessageBox.Show("Problem Importing File");
+
+                Bitmap b = new Bitmap(500, 500);
+                return Document.FromImage(b);
+            }
+        }
+
+        protected override void OnSave(Document input, Stream output, SaveConfigToken token,
+            Surface scratchSurface, ProgressEventHandler callback)
+        {
+            RenderArgs ra = new RenderArgs(new Surface(input.Size));
+            input.Render(ra, true);
+
+            ra.Bitmap.Save(output, ImageFormat.Bmp);
+        }
+    }
+
+    public class MyFileTypeFactory : IFileTypeFactory
+    {
+        public FileType[] GetFileTypeInstances()
+        {
+            return new FileType[] { new MyFileType() };
+        }
+    }
+}
